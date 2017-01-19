2418a316a165c66f388f153bd228a4a83ad5467c
Brenda Farrier and Keith Evans <keith@>
2017-01-19 10:06:05 -0800

    build initial file structure and some logic

---
+++ README.md
@@ -0,0 +1,47 @@
+# _Roman Numeral Translator_
+
+#### _An app to translate decimal numbers into Roman numerals, 01/19/2017_
+
+#### By _**Keith Evans**_
+
+## Description
+
+_This webpage allows user to enter any decimal value from 1 to 3,999. It then translates the user input into the Roman numeral numbering system._
+
+## Setup/Installation Requirements
+
+* _Clone GitHub repository to desktop_
+* _Open index.html in any browser_
+
+## Known Bugs
+
+__
+
+## Application Specs
+
+* The program will recognize non-numeric characters and values greater than 3,999 and respond.
+  * **Input Example:** a
+  * **Output Example:** That's not a number.
+* The program will recognize numbers with directly corresponding Roman numerals and translate them.
+  * **Input Example:** 100
+  * **Output Example:** C
+* The program will add more Roman Numerals in order to reach the specified value.
+  * **Input Example:** 8
+  * **Output Example:** VIII
+* The program will never repeat a numeral 4 times, and instead will subtract.
+  * **Input Example:** 9
+  * **Output Example:** IX
+
+## Support and contact details
+
+_Any questions, comments, or bug reports can be directed to the repository administrator._
+
+## Technologies Used
+
+_HTML/CSS/Bootstrap/JavaScript/jQuery_
+
+### License
+
+*This page is under the MIT License.*
+
+Copyright (c) 2017 **_Keith Evans_**
---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+//Back-end
+var numerals = {
+  "1": "I",
+  "5": "V",
+  "10": "X",
+  "50": "L",
+  "100": "C",
+  "500": "D",
+  "1000": "M"
+}
+
+var denominators = [1000, 500, 100, 50, 10, 5, 1]
+
+function buildNumeralsArray(num) {
+  var numOfEach = [];
+  denominators.forEach(function(denominator) {
+    var numOfNum = Math.floor(num/denominator);
+    num = num % (denominator * numOfNum);
+    numOfEach.push(numOfNum);
+  });
+  return numOfEach;
+}
+
+function addNumerals(num) {
+  var array = buildNumeralsArray(num);
+  var resultString = "";
+  for (var i = 0; i < array.length; i++) {
+    var matchingLetter = numerals[denominators[i].toString()];
+    for (var j = 0; j < array[i]; j++) {
+      resultString += matchingLetter;
+    }
+  }
+  return resultString;
+}
+
+function translate(num) {
+  if (!parseInt(num)) {
+    return "Please enter a number."
+  } else if (parseInt(num) < 1 || parseInt(num) > 3999) {
+    return "Please enter a number from 1 to 3,999."
+  } else {
+    var finalString = addNumerals(num);
+
+
+    return finalString;
+  }
+}
+
+//Front-end
+$(function() {
+  function displayAnswer(answer) {
+    $("#answer").text(answer);
+  }
+
+  $("#translator").submit(function(event) {
+    event.preventDefault();
+
+    var input = $("#translator input").val();
+    var answer = translate(input);
+
+    displayAnswer(answer);
+  });
+});
