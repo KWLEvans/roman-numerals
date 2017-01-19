f6eac016b46fd1c6f2d21da4576b125b9989e369
Brenda Farrier and Keith Evans <keith@>
2017-01-19 10:33:40 -0800

    add values for subtractionnumerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
 //Back-end
 var numerals = {
   "1": "I",
+  "4": "IV",
   "5": "V",
+  "9": "IX",
   "10": "X",
+  "40": "XL",
   "50": "L",
+  "90": "XC",
   "100": "C",
+  "400": "CD",
   "500": "D",
+  "900": "CM",
   "1000": "M"
 }
 
-var denominators = [1000, 500, 100, 50, 10, 5, 1]
+var denominators = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
 function buildNumeralsArray(num) {
   var numOfEach = [];
   denominators.forEach(function(denominator) {
     var numOfDenom = Math.floor(num/denominator);
-    console.log("numofnum is: " + numOfDenom);
     if (numOfDenom) {
       num = num % (denominator * numOfDenom);
       numOfEach.push(numOfDenom);
@@ -23,7 +28,6 @@ function buildNumeralsArray(num) {
       numOfEach.push(0);
     }
   });
-  console.log(numOfEach);
   return numOfEach;
 }
 
